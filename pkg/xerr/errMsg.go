ec77ad577defe354b074a1e00464ca34f7438274
Matthew-SD-Gibson <mattsdgibson@hotmail.com>
2025-02-22 10:18:35 -0500

    Update errMsg.go

--- pkg/xerr/errMsg.go
+++ pkg/xerr/errMsg.go
@@ -11,7 +11,7 @@ func init() {
 	message[EMAIL_UNREGISTERED_ERROR] = "User unregistered"
 	message[EMAIL_REGISTERD_BEFORE_ERROR] = "This email has already been registered"
 	message[USER_NOT_VERIFIED_ERROR] = "User not verified"
-	message[USER_PASSWORD_ERROR] = "Incorrect password"
+	message[USER_PASSWORD_ERROR] = "Email or password incorrect"
 }
 
 func MapErrMsg(code uint32) string {
