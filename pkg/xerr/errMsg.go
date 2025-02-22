e652b3d3e3be3779aaa1b2829967917daa6361d9
Matthew-SD-Gibson <mattsdgibson@hotmail.com>
2025-02-22 09:58:47 -0500

    Update errMsg.go

--- pkg/xerr/errMsg.go
+++ pkg/xerr/errMsg.go
@@ -5,13 +5,13 @@ var message map[uint32]string
 func init() {
 	message = make(map[uint32]string)
 	message[OK] = "success"
-	message[SERVER_COMMON_ERROR] = "server common error"
-	message[REQUEST_PARAM_ERROR] = "request parameter error"
-	message[TOKEN_INVALID_ERROR] = "token invalid"
-	message[EMAIL_UNREGISTERED_ERROR] = "email unregistered"
-	message[EMAIL_REGISTERD_BEFORE_ERROR] = "email register before"
-	message[USER_NOT_VERIFIED_ERROR] = "user not verified"
-	message[USER_PASSWORD_ERROR] = "password error"
+	message[SERVER_COMMON_ERROR] = "Server common error"
+	message[REQUEST_PARAM_ERROR] = "Request parameter error"
+	message[TOKEN_INVALID_ERROR] = "Token invalid"
+	message[EMAIL_UNREGISTERED_ERROR] = "User unregistered"
+	message[EMAIL_REGISTERD_BEFORE_ERROR] = "This email has already been registered"
+	message[USER_NOT_VERIFIED_ERROR] = "User not verified"
+	message[USER_PASSWORD_ERROR] = "Incorrect password"
 }
 
 func MapErrMsg(code uint32) string {
