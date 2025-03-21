99c3ee54804b3030f6ae8d6301371a7ddbac8644
Matthew-SD-Gibson <mattsdgibson@hotmail.com>
2025-03-21 18:21:46 -0400

    Update shopping_list.sql

--- newapp/shopping_list/shopping_list.sql
+++ newapp/shopping_list/shopping_list.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS shopping_list_items (
     id BIGINT NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
     ingredient_id BIGINT NOT NULL,
+    ingredient varchar(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
