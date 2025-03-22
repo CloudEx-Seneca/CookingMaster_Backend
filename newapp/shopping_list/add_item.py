559634e8084f223505bccdcfa8b2f6cb8ff22843
Matthew-SD-Gibson <mattsdgibson@hotmail.com>
2025-03-21 22:45:12 -0400

    Update add_item.py

--- newapp/shopping_list/add_item.py
+++ newapp/shopping_list/add_item.py
@@ -1,33 +1,23 @@
-from flask import Flask, request, jsonify
-import mysql.connector
-import yaml
+from database import get_db_connection
+import json
 
-# Load database config
-with open("shop_config.yaml", "r") as file:
-    config = yaml.safe_load(file)
+def add_to_shopping_list(data):
+    db = get_db_connection()
+    cursor = db.cursor()
 
-# Initialize Flask app
-app = Flask(__name__)
+    user_id = data["user_id"]
+    ingredient_id = data["ingredient_id"]
+    ingredient = data["ingredient"]
 
-# Connect to MySQL
-db = mysql.connector.connect(
-    host=config["db"]["host"],
-    user=config["db"]["user"],
-    password=config["db"]["password"],
-    database=config["db"]["database"]
-)
-cursor = db.cursor()
-
-# Function to add an item to the shopping list
-@app.route("/add_item", methods=["POST"])
-def add_to_shopping_list(user_id, ingredient_id):
     query = """
-    INSERT INTO shopping_list_items (user_id, ingredient_id)
-    VALUES (%s, %s)
+    INSERT INTO shopping_list_items (user_id, ingredient_id, ingredient)
+    VALUES (%s, %s, %s)
+    ON DUPLICATE KEY UPDATE ingredient = VALUES(ingredient)
     """
-    cursor.execute(query, (user_id, ingredient_id))
+    cursor.execute(query, (user_id, ingredient_id, ingredient))
     db.commit()
-    print("Item added to shopping list.")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    cursor.close()
+    db.close()
+
+    return {"message": "Item added to shopping list"}
